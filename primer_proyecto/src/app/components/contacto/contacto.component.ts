58dac7c302d8cf058329a54ecdf6ce06de276d0b
diego73821 <diego73821@gmail.com>
2022-10-13 21:03:17 -0500

    inicio

--- primer_proyecto/src/app/components/contacto/contacto.component.ts
+++ primer_proyecto/src/app/components/contacto/contacto.component.ts
@@ -1,43 +1,29 @@
-import { group } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Contacto } from 'src/app/models/contacto';
-
-
 
 @Component({
-  selector: 'app-contacto',
-  templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
+    selector: 'app-contacto',
+    templateUrl: './contacto.component.html',
+    styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
+    contactoForm: FormGroup;
+    regexCorreo = /\S+@\S+\.\S+/
 
-
-  contactoForm: FormGroup
-  regexCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
-  regexNumero = /^[0-9]/
-
-  constructor(private fb: FormBuilder) {
-    this.contactoForm = this.fb.group({
-      nombre: ['', Validators.required],
-      apellido: ['', Validators.required],
-      correo: ['', [Validators.required, Validators.pattern(this.regexCorreo)]],
-    })
-  }
-
-
-  ngOnInit(): void {
-  }
-
-  contactoUsuario(){
-    if (this.regexCorreo.test(this.contactoForm.get('correo')?.value)) {
-      
-    }else{
-      const DATA_USUARIO: Contacto ={
-        nombre: this.contactoForm.get('nombre')?.value,
-        apellido: this.contactoForm.get('apllido')?.value,
-        correo: this.contactoForm.get('correo')?.value,
-      }
+    constructor(private fb: FormBuilder) { 
+        this.contactoForm = this.fb.group({
+            correo: ['', [Validators.required, Validators.pattern(this.regexCorreo)]],
+            celular: ['', Validators.required],
+            asunto: ['', Validators.required],
+            mensaje: ['', Validators.required],
+        })
     }
-}
+
+    ngOnInit(): void {
+    }
+
+    contacto() {
+        console.log("Validación exitosa")
+    }
+
 }
